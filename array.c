762366d92cce6cceb3bcab70830b76c022141656
VidyashreeB.S <vidyashreev276@gmail.com>
2025-11-24 08:25:34 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,8 @@
+//program:Counting elements in an array
+#include<stdio.h>
+int main(){
+    int arr[]={10,20,30,40.50};
+    int count=size(arr/sizeof(arr[0]));
+    printf("Total number of element in the array=%d\n",count);
+    return 0;
+}
